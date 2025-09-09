59e47b05142ca7a61063ec1fd68c61896ee826d2
kalyan <mkalyan8400@gmail.com>
2025-09-09 13:26:35 +0530

    resume changed

--- src/main.jsx
+++ src/main.jsx
@@ -36,5 +36,5 @@ const router = createBrowserRouter([{
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>,  
 )
